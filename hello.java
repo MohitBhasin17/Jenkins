44f99fd7402ee659860856e018bc7cc062f0a288
MohitBhasin17 <bhasinm1791@gmail.com>
2026-03-09 01:06:42 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,6 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("update for webhook trigger");
+        System.out.println("Numbers addition: " + (500 + 60));
+    }
+}
